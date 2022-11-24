6cfeb38e1cb909466bb3bb03bad343c60aca5d01
Yinmu Liu <yinmu.liu@gmail.com>
2022-11-24 16:15:06 +1100

    add styling to footer

--- client/src/components/Home/Footer.js
+++ client/src/components/Home/Footer.js
@@ -1,9 +1,28 @@
+import classes from "./Footer.module.css";
+
 const Footer = () => {
   return (
-    <div>
-      <p>Footer Company Logo</p>
-      <p>Where to find us</p>
-      <p>Subscribe</p>
+    <div className={classes["main-footer"]}>
+      <div className={classes.container}>
+        <div className={classes.row}>
+          {/* Column1 */}
+          <div className={classes.col}>
+            <h4>Tee Shop</h4>
+          </div>
+          {/* Column2 */}
+          <div className={classes.col}>
+            <h4>Subscribe</h4>
+          </div>
+          {/* Column3 */}
+          <div className={classes.col}>
+            <h4>Where to find us?</h4>
+          </div>
+        </div>
+        <hr />
+        <div className={classes.row}>
+          <p className={classes.text}>@2022 Yinmu | Github | Linkedin</p>
+        </div>
+      </div>
     </div>
   );
 };
